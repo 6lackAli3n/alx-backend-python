cd27e385c7c63be4e7683edd8ecdc09ba073097b
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-08-16 00:28:05 +0100

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module for a simple addition function with type annotations.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floats together and returns the sum.
+    """
+    return a + b
+
